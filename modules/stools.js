bc833bf96333b0a9d4606a5b6230daad2cb8eed7
spout <snoopyxdy@gmail.com>
2011-10-14 16:58:00 +0800

    修改日期BUG

--- modules/stools.js
+++ modules/stools.js
@@ -2,7 +2,7 @@ var tools = {}
 tools.fdate = function(format){	
 	var format = typeof format === 'undefined'?false:format.toLowerCase(),
 		now = new Date(),
-		time = now.getFullYear()+"-"+(now.getMonth()+1)+"-"+now.getDay();
+		time = now.getFullYear()+"-"+(now.getMonth()+1)+"-"+now.getDate();
 	if(format === 'y-m-d h:m:s'){
 		time += ' '+now.getHours()+":"+now.getMinutes()+":"+now.getSeconds(); 
 	}
