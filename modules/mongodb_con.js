ab8b0891418bd6368d21bcf73643b01cc4a93af7
spout <snoopyxdy@gmail.com>
2011-12-21 09:24:44 +0800

    //增加数据库连接失败错误日志

--- modules/mongodb_con.js
+++ modules/mongodb_con.js
@@ -6,14 +6,15 @@ var poolModule = require('generic-pool'),
 var pool = poolModule.Pool({
         name     : 'mongodb',
         create   : function(callback) {
-					var dbserver =  new Server("127.0.0.1", 27017, {});
+					var dbserver =  new Server("10.1.80.2", 27017, {});
 					var client = new Db('spout', dbserver, {});
 				   client.open(function(err, db){
-						 callback(null, db);  //创建完了以后，要把client传给callback，这里第一个参数看了源码得知是err，我们不必理会
+					   if(err)  _logger.error('数据库连接失败：'+err);
+					   else callback(null, db);  //创建完了以后，要把client传给callback，这里第一个参数看了源码得知是err，我们不必理会
 				   })
         },
         destroy  : function(db) { db.close(); }, //当超时则释放连接
-        max      : 1,   //最大连接数
+        max      : 100,   //最大连接数
         idleTimeoutMillis : 1000*30,  //超时时间
         log : false,  
     });
